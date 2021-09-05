1ab1bc0d9c2c9327df561648ded24feff65ef15f
anuj108 <shakya.anuj910@gmail.com>
2021-09-05 20:02:35 +0530

    minor fixes

--- calculator.js
+++ calculator.js
@@ -1,23 +1,19 @@
-let screen=document.getElementById('abc')
-buttons= document.querySelectorAll('button');
-let screenValue='';
-for(item of buttons){
-    item.addEventListener('click',(e)=>{
-        buttonText=e.target.innerText;
-        console.log('button text is',buttonText);
-        if(buttonText=='=')
-        {
-            screen.value=eval(screenValue);
-        }
-        else if(buttonText=='CLEAR')
-        {
-            screen.value="";
-            screenValue=screen.value;
-        }
-        else{
-            screenValue +=buttonText;
-            console.log(screenValue);
-            screen.value=screenValue;
-        }
-    })
+let screen = document.getElementById("abc");
+buttons = document.querySelectorAll("button");
+let screenValue = "";
+for (item of buttons) {
+  item.addEventListener("click", (e) => {
+    buttonText = e.target.innerText;
+    console.log("button text is", buttonText);
+    if (buttonText == "=") {
+      screen.value = eval(screenValue);
+    } else if (buttonText == "CLEAR") {
+      screen.value = "";
+      screenValue = screen.value;
+    } else {
+      screenValue += buttonText;
+      console.log(screenValue);
+      screen.value = screenValue;
+    }
+  });
 }
